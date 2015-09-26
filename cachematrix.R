c3b0d9d364cb87d3aa2ac46ec5519387a381a80e
Zuzah <murtaza.hasni@gmail.com>
2015-09-26 17:44:26 -0400

    initial commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,17 @@
+#Name: Murtaza Hasni
+#Date: September 23, 2015
+#Course: RPROG-032 - R Programming
+
 ## Put comments here that give an overall description of what your
 ## functions do
 
 ## Write a short comment describing this function
-
 makeCacheMatrix <- function(x = matrix()) {
 
 }
 
 
 ## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
 }
