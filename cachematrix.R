0792f42b1f3be1bb6544158ca1ea96ab4c25fd65
Zuzah <murtaza.hasni@gmail.com>
2015-09-26 22:03:46 -0400

    replaced mean() function with solve() function to produce inverse matrix. Also added comments in how to use program

--- cachematrix.R
+++ cachematrix.R
@@ -2,11 +2,23 @@
 #Date: September 23, 2015
 #Course: RPROG-032 - R Programming
 
-#Note: the assignmen2 functions will be modified
-#      to cache a matrix's inverse rather than caching the mean
+#Note: The functions were modified from the ones found on assignment2's README.md
+#      The function will cache a matrix's inverse (instead of caching the mean).
+#      The solve() function was used as per line 84 of the README.md
+
+#Usage:
+#1. Source program in R:
+#                       > source("cachematrix.R")
+#2. Construct a Matrix in R:
+#                       > matx <- matrix( c(3, 0, 0, 0, 3, 0, 0, 0, 3), nrow=3, ncol=3)
+#3. Supply your matrix to makeCacheMatrix(); assign it to a cached matrix var:
+#                       > cachedata <- makeCacheMatrix(matx)
+#6. Supply the cacheSolve() function with your cache data var:
+#                       >cacheSolve(cachedata)
+#5. Compare your original matrix with the result of cacheSolve which generates its inverse
 
 
-##Function that takes a matrix parm and then cache its inverse
+#Function that takes a matrix parm and then cache its inverse
 makeCacheMatrix <- function(x = matrix()) { #changed from numeric() to matrix() from template
 
   #Used the assignment2 pages 'makevector' function as a template
@@ -16,11 +28,11 @@ makeCacheMatrix <- function(x = matrix()) { #changed from numeric() to matrix() 
                m <<- NULL
        }
        get <- function() x
-       setmean <- function(mean) m <<- mean #TODO: replace with a set inverse var
-       getmean <- function() m  #TODO: replace with a get inverse var
+       setsolve <- function(solve) m <<- solve #replaced mean() with solve() to cal inverse
+       getsolve <- function() m  #TODO: replace with a get inverse var
        list(set = set, get = get,
-            setmean = setmean,  #TODO: replace with a set inverse assignment
-            getmean = getmean)  #TODO: replace with a get inverse assignment
+            setsolve = setsolve,  #TODO: replace with a set inverse assignment
+            getsolve = getsolve)  #TODO: replace with a get inverse assignment
 
 }
 
@@ -31,13 +43,13 @@ cacheSolve <- function(x, ...) {
 
         #Used the assignment2 pages 'cachemean' function as a template
 
-        m <- x$getmean()
+        m <- x$getsolve()
         if(!is.null(m)) {
                 message("getting cached data")
                 return(m)
         }
         data <- x$get()
-        m <- mean(data, ...)
-        x$setmean(m)
+        m <- solve(data, ...) #change mean() to solve()
+        x$setsolve(m)
         m
 }
